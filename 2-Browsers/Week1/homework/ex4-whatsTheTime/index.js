c661039cb7dfc7fd084f43a7cee5371c41104ba3
Mahmood <pejvakco@gmail.com>
2021-02-28 00:27:31 +0100

    Finished 1, 2, 4, 5

--- 2-Browsers/Week1/homework/ex4-whatsTheTime/index.js
+++ 2-Browsers/Week1/homework/ex4-whatsTheTime/index.js
@@ -7,6 +7,53 @@
 ------------------------------------------------------------------------------*/
 function addCurrentTime() {
   // TODO complete this function
+  const date = new Date();
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
+  let session = 'AM';
+
+  if (h === 0) {
+    h = 12;
+  }
+
+  if (h > 12) {
+    h = h - 12;
+    session = 'PM';
+  }
+
+  h = h < 10 ? '0' + h : h;
+  m = m < 10 ? '0' + m : m;
+  s = s < 10 ? '0' + s : s;
+
+  const time = h + ':' + m + ':' + s + ' ' + session;
+
+  const divClockContainer = document.getElementById('divClockContainer');
+  divClockContainer.innerText = time;
 }
 
 // TODO execute `addCurrentTime` when the browser has completed loading the page
+
+function createDom() {
+  const body = document.querySelector('body');
+  body.style.background = 'black';
+  const divClockContainer = document.createElement('div');
+  divClockContainer.id = 'divClockContainer';
+  divClockContainer.style.position = 'absolute';
+  divClockContainer.style.top = '50%';
+  divClockContainer.style.left = '50%';
+  divClockContainer.style.transform = 'translateX(-50%) translateY(-50%)';
+  divClockContainer.style.color = '#17D4FE';
+  divClockContainer.style.fontSize = '60px';
+  divClockContainer.style.fontFamily = 'Orbitron';
+  divClockContainer.style.letterSpacing = '7px';
+  body.appendChild(divClockContainer);
+}
+
+function initialize() {
+  createDom();
+  addCurrentTime();
+  setInterval(addCurrentTime, 1000);
+}
+
+window.addEventListener('load', initialize);

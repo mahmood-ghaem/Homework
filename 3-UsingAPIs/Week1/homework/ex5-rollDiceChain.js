ffbf23c99a507d174495c44c666b56ee2ded62c7
Mahmood <pejvakco@gmail.com>
2021-03-22 19:31:14 +0100

    update ex5

--- 3-UsingAPIs/Week1/homework/ex5-rollDiceChain.js
+++ 3-UsingAPIs/Week1/homework/ex5-rollDiceChain.js
@@ -6,24 +6,18 @@ function rollTheDices() {
   // TODO: expand the chain to include five dices
   rollDice(1)
     .then((value) => {
-      console.log('value: ', value);
       results.push(value);
       return rollDice(2);
     })
     .then((value) => {
-      console.log('value: ', value);
-
       results.push(value);
       return rollDice(3);
     })
     .then((value) => {
-      console.log('value: ', value);
-
       results.push(value);
       return rollDice(4);
     })
     .then((value) => {
-      console.log('value: ', value);
       results.push(value);
       return rollDice(5);
     })
@@ -32,21 +26,24 @@ function rollTheDices() {
 }
 rollTheDices();
 //===================================================================
+const rollDice = require('../../helpers/pokerDiceRoller');
+
 function rollTheDicesDry() {
   const results = [];
-
-  // pushAndRoll is two function value filled by previous call pushAndRoll by return
+  // pushAndRoll is two arrow functions. 'value' filled by previous call pushAndRoll by return
   const pushAndRoll = (dice) => (value) => {
     results.push(value);
     return rollDice(dice);
   };
-
   rollDice(1)
     .then(pushAndRoll(2))
     .then(pushAndRoll(3))
     .then(pushAndRoll(4))
     .then(pushAndRoll(5))
-    .then(() => console.log('Resolved!', results))
+    .then((value) => {
+      results.push(value);
+      console.log('Resolved!', results);
+    })
     .catch((error) => console.log('Rejected!', error.message));
 }
 rollTheDicesDry();
